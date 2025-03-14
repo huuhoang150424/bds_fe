21482d6b2fa9fed11fdcbd92b70568f42f589929
huuhoang150424 <nguyenhoanghuu15042004@gmail.com>
2025-03-14 23:31:35 +0700

    fix: dashbroad screen

--- src/screen/admin/dasbroad/components/char.tsx
+++ src/screen/admin/dasbroad/components/char.tsx
@@ -1,4 +1,7 @@
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts'
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue, } from '@/components/ui/select';
+import { useState } from 'react';
 
 const data = [
   { name: 'Jan', total: 12000000 },
@@ -49,4 +52,36 @@ const Overview: React.FC = () => {
   )
 }
 
-export default Overview;
+const Chart: React.FC=()=> {
+
+  const [sizePage, setSizePage] = useState(2025);
+  
+  return (
+    <Card className='self-start col-span-1 lg:col-span-4 rounded-[6px] border border-gray-200 shadow-sm cursor-pointer dark:bg-colorDarkMode dark:border-borderDarkMode transition-all duration-500 ease-linear'>
+    <CardHeader className='flex flex-row items-center justify-between '>
+      <CardTitle>Doanh thu của từng tháng</CardTitle>
+      <Select
+          value={`${sizePage}`}
+          onValueChange={(value) => {
+            setSizePage(Number(value))
+          }}
+        >
+          <SelectTrigger className='h-8 w-[70px]  shadow-none '>
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent side='top'>
+            {[2020, 2021, 2022, 2023, 2024,2025].map((pageSize) => (
+              <SelectItem className="text-textColor" key={pageSize} value={`${pageSize}`}>
+                {pageSize}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+    </CardHeader>
+    <CardContent className='pl-2'>
+      <Overview />
+    </CardContent>
+  </Card>
+  )
+}
+export default Chart;
