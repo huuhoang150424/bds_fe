fa8d2166b9b474a613dc607904011eb5246af99a
ducmanh1504 <nguyenducmanh0228@gmail.com>
2025-04-07 14:33:49 +0700

    feat:pagination post

--- src/page/user/sellDetails/components/property-listing.tsx
+++ src/page/user/sellDetails/components/property-listing.tsx
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom";
-import PropertyCard from "./property-card";
-import { useGetListPosts } from "../hooks/use-get-list-post";
-import { useState } from "react";
+import { Link } from 'react-router-dom';
+import PropertyCard from './property-card';
+import { useGetListPosts } from '../hooks/use-get-list-post';
+import { useState } from 'react';
+import { PaginationPost } from './pagination';
 
 interface PropertyListingsProps {
   realEstateListings: any[];
@@ -51,30 +52,34 @@ interface Post {
   status: string;
   slug: string;
   images: Image[];
-  user : User;
+  user: User;
 }
 export default function PropertyListings() {
   const [page, setPage] = useState(1);
   const { data, isLoading, isError } = useGetListPosts(5, page);
-  console.log("data", data);
+  console.log('data', data);
+  console.log('total Page:', data?.totalPages);
 
   return (
-    <div className="post w-full lg:w-[75%] p-[15px] overflow-y-auto">
-      <div className="post__title mb-[30px]">
-        <h2 className="text-[22px] font-[650] mb-[10px]">Mua bán bất động sản trên toàn quốc</h2>
-        <p className="font-[400] text-[14px] text-sm">Hiện có  bất động sản trên toàn quốc</p>
+    <div className='post w-full lg:w-[75%] p-[15px] overflow-y-auto'>
+      <div className='post__title mb-[30px]'>
+        <h2 className='text-[22px] font-[650] mb-[10px]'>Mua bán bất động sản trên toàn quốc</h2>
+        <p className='font-[400] text-[14px] text-sm'>Hiện có bất động sản trên toàn quốc</p>
       </div>
-      <div className="post__detail flex flex-col gap-6">
+      <div className='post__detail flex flex-col gap-6'>
         {isLoading ? (
           <p>Đang tải...</p>
         ) : isError ? (
           <p>Có lỗi xảy ra</p>
         ) : (
-          data?.posts?.map((post: Post) => (
-            <Link key={`${post?.id}-index`} to={`/post/${post?.slug}`}>
-              <PropertyCard {...post} />
-            </Link>
-          ))
+          <>
+            {data?.posts?.map((post: Post) => (
+              <Link key={`${post?.id}-index`} to={`/post/${post?.slug}`}>
+                <PropertyCard {...post} />
+              </Link>
+            ))}
+            <PaginationPost page={page} totalPages={data?.totalPages} setPage={setPage} />
+          </>
         )}
       </div>
     </div>
